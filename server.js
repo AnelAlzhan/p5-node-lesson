e8beae52aa037825d02d24de53dc6bcc613f69a2
AnelAlzhan <anelya99@gmail.com>
2021-11-17 11:33:47 +0100

    added sketch; p5 project created and is working

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 console.log("up and running");
+
+const { prototype } = require("events");
+let express = require("express");
+
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+console.log("server is runninig on http://localhost:" + port);
+
+app.use(express.static("public"));
